bf083653686e9a660fb579b5714d907894a1036c
creaciond <daria.maximova.m@gmail.com>
2018-02-02 00:06:49 +0300

    add article class

--- semantics/semsite/models.py
+++ semantics/semsite/models.py
@@ -1,3 +1,12 @@
 from django.db import models
 
-# Create your models here.
+
+class Article(models.Model):
+    title = models.Model.CharField(max_length=200)
+    main_image = models.ImageField(upload_to="/uploads/")
+    text = models.TextField()
+    literature = models.ManyToManyField(Publication)
+    ideas = models.ManyToManyField(Idea)
+    terms = models.ManyToManyField(Term)
+    # картиночки
+
