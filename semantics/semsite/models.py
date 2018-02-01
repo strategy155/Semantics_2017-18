d93f9a182218a67e3a6ffe5aac7f69793e408633
creaciond <daria.maximova.m@gmail.com>
2018-02-02 00:07:22 +0300

    Revert "add article class"
    This reverts commit bf083653686e9a660fb579b5714d907894a1036c.

--- semantics/semsite/models.py
+++ semantics/semsite/models.py
@@ -1,12 +1,3 @@
 from django.db import models
 
-
-class Article(models.Model):
-    title = models.Model.CharField(max_length=200)
-    main_image = models.ImageField(upload_to="/uploads/")
-    text = models.TextField()
-    literature = models.ManyToManyField(Publication)
-    ideas = models.ManyToManyField(Idea)
-    terms = models.ManyToManyField(Term)
-    # картиночки
-
+# Create your models here.
