41473bbd6d071c5da1c90ee1068fb4838ea5553b
GaurwaithMelui <strategy155@gmail.com>
2018-02-02 00:17:30 +0300

    added fields: Idea Publication Person Term Dictionary added .gitignore

--- semantics/semsite/models.py
+++ semantics/semsite/models.py
@@ -1,3 +1,42 @@
 from django.db import models
 
 # Create your models here.
+
+
+# class Translation(models.Model):
+#     language = models.CharField(max_length=200)
+#     text = models.CharField(max_length=200)
+
+
+class IdeaDescriptor:
+    def __get__(self, instance, owner):
+        return [pub.ideas.all() for pub in instance.publications.all()]
+
+
+class Term(models.Model):
+    name = models.CharField(max_length=200)
+    #translations = models.ManyToManyField(Translation)
+    translations = models.TextField()
+    description = models.TextField()
+
+
+class Dictionary(models.Model):
+    terms = models.ManyToManyField(Term)
+
+
+class Idea(models.Model):
+    name = models.CharField(max_length=30)
+
+
+class Publication(models.Model):
+    name = models.CharField(max_length=30)
+    ideas = models.ManyToManyField(Idea)
+
+
+class Person(models.Model):
+    first_name = models.CharField(max_length=30)
+    last_name = models.CharField(max_length=30)
+    bio = models.TextField()
+    photo = models.ImageField(upload_to='uploads/')
+    publications = models.ManyToManyField(Publication)
+    ideas = IdeaDescriptor()
