40a57dabdaf8f3392c6bf7032cfd4cd084a07460
deltamachine <an-an-kondratjeva@yandex.ru>
2018-02-08 12:21:16 +0300

    little fixes + side nav

--- semantics/semsite/models.py
+++ semantics/semsite/models.py
@@ -41,8 +41,8 @@ class Person(models.Model):
 
 
 class Article(models.Model):
-    title = models.Model.CharField(max_length=200)
-    main_image = models.ImageField(upload_to="/uploads/")
+    title = models.CharField(max_length=200)
+    main_image = models.ImageField(upload_to="uploads/")
     text = models.TextField()
     literature = models.ManyToManyField(Publication)
     ideas = models.ManyToManyField(Idea)
