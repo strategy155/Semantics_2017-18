0d8c7abb86aaafa995f9df93e11b9a2153953b78
Mark Sobolev <strategy155@gmail.com>
2018-02-11 14:27:17 +0300

    changed db back

--- semantics/semantics/settings.py
+++ semantics/semantics/settings.py
@@ -76,8 +76,12 @@ WSGI_APPLICATION = 'semantics.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': 'semsite_db',
+        'USER': 'gwm',
+        'PASSWORD': 'gwm',
+        'HOST': 'localhost',
+        'PORT': '',
     }
 }
 
@@ -119,3 +123,4 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/1.11/howto/static-files/
 
 STATIC_URL = '/static/'
+STATIC_ROOT = '/semsite/static/'
