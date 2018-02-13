27da03a8f908cb3d4dfefa18658b5d60d6499ad4
strategy155 <strategy155@gmail.com>
2018-02-13 16:58:11 +0000

    edited secret key at depl

--- semantics/semantics/settings.py
+++ semantics/semantics/settings.py
@@ -20,7 +20,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # See https://docs.djangoproject.com/en/1.11/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = '=w7yqpjxy7k369gocj@*wa#9z%yvjbcog9_$-r6%zgkvtbt9j+'
+SECRET_KEY = os.environ['DJANGO_SECRET_KEY']
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
