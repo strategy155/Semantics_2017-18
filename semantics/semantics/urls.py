c6c9b0893180f39d2d0f60d2b87dbf3786780bba
Mark Sobolev <strategy155@gmail.com>
2018-02-13 22:05:00 +0300

    fixed problems with views and urls logic unconsistently processed by templates

--- semantics/semantics/urls.py
+++ semantics/semantics/urls.py
@@ -16,7 +16,7 @@ Including another URLconf
 from django.urls import path
 from django.conf.urls import url, include
 from django.contrib import admin
-from semsite.views import IndexView, HandbookView, HandbookArticleView, AuthorView
+from semsite.views import IndexView, HandbookView, AuthorView
 from django.conf import settings
 from django.conf.urls.static import static
 
@@ -28,7 +28,6 @@ urlpatterns = [
     url(r'^$', IndexView.as_view(), name='index'),
     path('authors/', AuthorView.as_view(), name='authors'),
     path('handbook/', HandbookView.as_view(), name='handbook'),
-    path('handbook/<str:urlname>/', HandbookArticleView.as_view(), name='handbook_article'),
     url(r'^tinymce/', include('tinymce.urls'))
 
 ]  + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
