257e4dc86eaaa0423d506c008599222f4647a00c
Mark Sobolev <strategy155@gmail.com>
2018-02-14 19:08:31 +0300

    typo

--- semantics/semantics/urls.py
+++ semantics/semantics/urls.py
@@ -29,7 +29,7 @@ urlpatterns = [
     path('authors/', AuthorView.as_view(), name='authors'),
     path('handbook/', HandbookView.as_view(), name='handbook'),
     path('dictionary/', DictionaryView.as_view(), name='dictionary'),
-    path('literacture/', LiteratureView.as_view(), name='literature'),
+    path('literature/', LiteratureView.as_view(), name='literature'),
     url(r'^publications/', include('publications_bootstrap.urls')),
     url(r'^ckeditor/', include('ckeditor_uploader.urls')),
 ]  + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
