4de4df83857c46c139727f0632a91ba75dcdc2f7
Mark Sobolev <strategy155@gmail.com>
2018-03-19 14:35:08 +0300

    gitignore revision

---
+++ semantics/semsite/static/semsite/js/basic_functions.js
@@ -0,0 +1,14 @@
+$(document).ready(function(){  
+    $(".button-collapse").sideNav();
+
+    /*$(window).scroll(function() {
+        scrollTop = $(window).scrollTop();
+        console.log('kek');
+        
+        if (scrollTop >= 55) {
+            $('.menu-button').css('color', '#29283f')
+        } else {
+            $('.menu-button').css('color', 'white')
+        }
+    }); */
+});
